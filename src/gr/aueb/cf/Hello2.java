a080e11dc3c881c32dcd945718687c7b53f2ec02
Konstantinos Charisis <harisis_kostas@hotmail.com>
2024-05-21 20:40:14 +0300

    Hello2 tested and changed

--- src/gr/aueb/cf/Hello2.java
+++ src/gr/aueb/cf/Hello2.java
@@ -3,6 +3,6 @@ package gr.aueb.cf;
 public class Hello2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello Again!");
+        System.out.println("Hello Again!!!!!!");
     }
 }
